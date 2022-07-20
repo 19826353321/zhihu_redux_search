54ceff08e165cc58dca19a3b9ec54a202881cf1f
YXF <10826319+yxf5663@user.noreply.gitee.com>
2022-07-20 21:07:25 +0800

    zhihu_search_redux

--- src/pages/Search/SearchFind/style.js
+++ src/pages/Search/SearchFind/style.js
@@ -13,29 +13,29 @@ export const SearchFindWrapper = styled.div`
         }
         .header-right{
            
-            &:hover>.icon-weibiaoti--{
+            /* &:hover>.icon-weibiaoti--{
                 @keyframes changerotate{
                 
                 0%{
-                /* background: red; */
+       
                 transform:rotate(0deg);
                 -webkit-transform: rotate(0deg);
-                /* -webkit-transform:rotate(0deg) */
+             
                 
                 }
                 100%{
-                    /* background: green; */
+             
                     transform:rotate(300deg);
                     -webkit-transform: rotate(300deg);
                 }
             }
                 animation: changerotate 1.4s ease .1s;
                 animation-fill-mode:both;
-                /* animation-iteration-count:infinite; */
-            }
+           
+            } */
             .icon-weibiaoti--{               
                 margin: 0 0.3rem;
-                /* animation-iteration-count:5; */
+              
                
                
                 
